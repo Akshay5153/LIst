6359cf0cbd21d9b9e06948c4edc08fe2fb133a63
Akshay5153 <akshaylonkar5153@gmail.com>
2024-12-24 19:06:42 +0530

    Add files via upload
    Convert to String to Integer and Float.py

---
+++ Convert to String to Integer and Float.py
@@ -0,0 +1,11 @@
+# Convert to String to Integer and Float
+String = input("Enter a Number") 
+
+# Casting to Integer
+Integer = int(String)
+
+# Casting to Float
+Float = float(String)
+
+print("String Converted to Integer", Integer)
+print("String Converted to Float", Float)
